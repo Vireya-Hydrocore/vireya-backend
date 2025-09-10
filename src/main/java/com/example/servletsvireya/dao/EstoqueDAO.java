ce741783043321359579230fff11a05f926c5c53
Kireway <erik.silva@germinare.org.br>
2025-09-10 20:03:35 -0300

    Elaboração da classe EstoqueDAO, com verificação de existência do ID

--- src/main/java/com/example/servletsvireya/dao/EstoqueDAO.java
+++ src/main/java/com/example/servletsvireya/dao/EstoqueDAO.java
@@ -125,7 +125,7 @@ public class EstoqueDAO { //erik
 
         } catch (SQLException e){
             e.printStackTrace();
-            //Retorna algo?
+            return null; //ta certo?????
         } finally {
             conexao.desconectar();
         }
