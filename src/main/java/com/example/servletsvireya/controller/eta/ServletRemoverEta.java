5f68ce83657e97eb475e5f184bed455db5d73b26
Kireway <erik.silva@germinare.org.br>
2025-09-11 12:38:36 -0300

    Corrigindo parte do servlet remover Eta

--- src/main/java/com/example/servletsvireya/controller/eta/ServletRemoverEta.java
+++ src/main/java/com/example/servletsvireya/controller/eta/ServletRemoverEta.java
@@ -19,7 +19,7 @@ public class ServletRemoverEta extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        // Pega o id da URL (ex: servlet-remover-eta?id=3)
+        // Pega o id da URL
         int id = Integer.parseInt(req.getParameter("id"));
 
         // Cria um objeto Eta só com o id
