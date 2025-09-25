8fc8f15583dab68c6c14ec46abdbdbd4a0139515
Kireway <erik.silva@germinare.org.br>
2025-09-25 14:45:36 -0300

    Arrumado setIdCargo()

--- src/main/java/com/example/servletsvireya/model/Funcionario.java
+++ src/main/java/com/example/servletsvireya/model/Funcionario.java
@@ -12,7 +12,8 @@ public class Funcionario {
 
     // Construtores
     public Funcionario(){}
-    public Funcionario(Funcionario funcionario){ //?
+    public Funcionario(int id, String nome, String email, LocalDate dataAdmissao,
+                       LocalDate dataNascimento, int idETA, int idCargo){
         this.id = id;
         this.nome = nome;
         this.email = email;
@@ -37,6 +38,6 @@ public class Funcionario {
     public void setDataAdmissao(LocalDate dataAdmissao){ this.dataAdmissao = dataAdmissao; }
     public void setDataNascimento(LocalDate dataNascimento){ this.dataNascimento = dataNascimento; }
     public void setIdEta(int idETA){ this.idETA = idETA; }
-    public void setIdCargo(int idFuncionario){ this.idCargo = idCargo; } //?
+    public void setIdCargo(int idCargo){ this.idCargo = idCargo; }
 
 }
